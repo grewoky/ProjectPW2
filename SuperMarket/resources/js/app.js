efc834e9df20bf8ff8a45bbb9b28696791c5c12f
Klaudius-Netizen <klaudiusaudieirsansaputra_2327250018@mhs.mdp.ac.id>
2025-01-05 00:09:16 +0700

    update

--- SuperMarket/resources/js/app.js
+++ SuperMarket/resources/js/app.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import App from './components/Example';
 import ReactDOM from 'react-dom/client'; // React 18 uses this
+import App from './components/Example'; // Ensure this is the correct path
 require('./bootstrap');
-require('./components/Example');
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCAuEhYZdoJQyPxYQ0dOYxgrl-r6SYha3I",
   authDomain: "supermarketproject-8bad3.firebaseapp.com",
   projectId: "supermarketproject-8bad3",
-  storageBucket: "supermarketproject-8bad3.firebasestorage.app",
+  storageBucket: "supermarketproject-8bad3.appspot.com", // Corrected storage bucket URL
   messagingSenderId: "856199745624",
   appId: "1:856199745624:web:5f06dbc3a6a65e97ae3a2d",
   measurementId: "G-TX8M6CSQ5W"
@@ -29,6 +26,5 @@ const analytics = getAnalytics(app);
 const rootElement = document.getElementById('example');
 if (rootElement) {
     const root = ReactDOM.createRoot(rootElement); // Create root for React 18
-    root.render(<Example />);
+    root.render(<App />); // Render the App component
 }
-ReactDOM.render(<App />, document.getElementById('example'));
--- SuperMarket/resources/views/welcome.blade.php
+++ SuperMarket/resources/views/welcome.blade.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel with React</title>
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">  <!-- Compiled CSS -->
 </head>
 <body>
-    <div id="example"></div>
+    <div id="example"></div>  <!-- React will render here -->
+    
     <h5>WEB BABI</h5>
-    <script src="{{ mix('js/app.js') }}"></script>
+
+    <script src="{{ mix('js/app.js') }}"></script>  <!-- Compiled JS -->
 </body>
 </html>
