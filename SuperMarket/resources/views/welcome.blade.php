3bc3964bbb963ee0b523667ff188def08bdfb179
Klaudius-Netizen <klaudiusaudieirsansaputra_2327250018@mhs.mdp.ac.id>
2025-01-09 01:56:10 +0700

    jsx

--- SuperMarket/resources/views/welcome.blade.php
+++ SuperMarket/resources/views/welcome.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel with React</title>
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">  <!-- Compiled CSS -->
+    <link href="{{ mix('resources/css/app.css') }}" rel="stylesheet">  <!-- Compiled CSS -->
 </head>
 <body>
     <div id="example"></div>  <!-- React will render here -->
     
     <h5>WEB BABI</h5>
 
-    <script src="{{ mix('js/app.js') }}"></script>  <!-- Compiled JS -->
+    <script type="module" src="{{ mix('resources/js/app.jsx') }}"></script>  <!-- Compiled JS -->
 </body>
 </html>
